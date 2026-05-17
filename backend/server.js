727ef04886e47c4b6b3a7ea6b92505b3049d2365
alvian <viankalvian055gmail.com>
2026-05-17 14:57:53 +0800

    final deploy

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mysql from 'mysql2';
 import cors from 'cors';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -14,10 +17,11 @@ KONEKSI DATABASE
 */
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'adminPenjualanHandphone'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
