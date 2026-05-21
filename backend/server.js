979442ad8295edf20339f4b3332aad3db60b019b
alvian <viankalvian055gmail.com>
2026-05-21 19:55:53 +0800

    memperbaiki masalah crashed di backend dan sedikit mengubah app listen port di server.js

--- backend/server.js
+++ backend/server.js
@@ -473,6 +473,8 @@ JALANKAN SERVER
 =====================================
 */
 
-app.listen(3000, () => {
-  console.log('Server running di port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running di port ${PORT}`);
 });
